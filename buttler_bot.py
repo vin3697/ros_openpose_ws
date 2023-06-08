a222c21f0e1b44b99c544e12e71d8e8e785e277c
Vinayak Patil <92587549+vin3697@users.noreply.github.com>
2023-06-08 22:04:16 +0200

    added_new_gestures_with_controls

--- buttler_bot.py
+++ buttler_bot.py
@@ -4,7 +4,7 @@
 # ! coomented visualizer.py node and echo.py node in core.launch inside docker container named vibrant_buck!!!
 # import modules
 import rospy
-from ros_openpose.msg import Frame 
+from ros_openpose.msg import Frame , Gesture
 from geometry_msgs.msg import Twist
 
 
@@ -20,62 +20,138 @@ LElbow      = 6 # Left Elbow
 LWrist      = 7 # Left Wrist
 
 MidHip      = 8 # Mid Hip as center of persons body
+RHip        = 9 # Right Hip
+LHip        = 12# Left Hip 
+
+class movements():
+    def __init__(self):
+
+        #! rospy.init_node('robot_control', anonymous=True) https://answers.ros.org/question/371673/error-rospyinit_node-has-already-been-called-with-different-arguments/
+        self.pub = rospy.Publisher('/tb_cmd_vel', Twist, queue_size=10) #? tb_cmd_vel rostopic for BOT
+        self.rate = rospy.Rate(10)  # 10Hz publishing rate
+
+    def forward_movement(self):
+        twist = Twist() 
+        twist.linear.x = 0.1
+        twist.linear.y = 0  #? to lateral movement but Bot doesnt support this movement
+        twist.angular.z = 0 #? no angular movement
+        self.pub.publish(twist)
+        #self.rate.sleep() #! goes into infinte sleep
+
+    def backward_movement(self):
+        twist = Twist() 
+        twist.linear.x = -0.1
+        twist.linear.y = 0
+        twist.angular.z = 0
+        self.pub.publish(twist)
+
+    def turn_right(self):
+        twist = Twist()
+        twist.angular.z = -0.1
+        twist.linear.x = 0
+        twist.linear.y = 0
+        self.pub.publish(twist)
+
+    def turn_left(self):
+        twist = Twist()
+        twist.angular.z = 0.1
+        twist.linear.x = 0.0
+        twist.linear.y = 0.0
+        self.pub.publish(twist)
+
+    def stop(self):
+        twist = Twist()
+        twist.linear.x = 0
+        twist.linear.y = 0
+        twist.angular.z = 0
+        self.pub.publish(twist)
+
+    def forward_right_turn(self):
+        twist = Twist()
+        twist.linear.x = 0.1
+        twist.linear.y = 0
+        twist.angular.z = -0.1
+        self.pub.publish(twist)
+
+    def forward_left_turn(self):
+        twist = Twist()
+        twist.linear.x = 0.1
+        twist.linear.y = 0
+        twist.angular.z = 0.1
+        self.pub.publish(twist)
+
+class all_Gestures():
+    #! add probability and distance conditon in each statement accordingly!! 
+    def go_forward_gesture(self, person):
+        return ((((person.bodyParts[LShoulder].pixel.y > person.bodyParts[LWrist].pixel.y > 0)
+                    or (person.bodyParts[RShoulder].pixel.y > person.bodyParts[RWrist].pixel.y > 0))
+                    and not ((person.bodyParts[LShoulder].pixel.y > person.bodyParts[LWrist].pixel.y > 0)          #not both hand raised!
+                            and (person.bodyParts[RShoulder].pixel.y > person.bodyParts[RWrist].pixel.y > 0)))
+                    and ((person.bodyParts[RShoulder].pixel.x > person.bodyParts[RWrist].pixel.x > 0)
+                        or (person.bodyParts[LWrist].x > person.bodyParts[LShoulder].pixel.x > 0))
+                    and ((abs(person.bodyParts[RWrist].pixel.x - person.bodyParts[RElbow].pixel.x) < 100)           # this is the width/distance on x axis
+                        or (abs(person.bodyParts[LWrist].pixel.x - person.bodyParts[LElbow].pixel.x) < 100)))       # !elbow and wrist should be in line   
 
 
+    def go_backward_gesture(self, person):
+
+        return ((person.bodyParts[RElbow].pixel.y > person.bodyParts[RShoulder].pixel.y > 0) 
+                    and (person.bodyParts[RElbow].pixel.y > person.bodyParts[RWrist].pixel.y > 0)
+                    and (person.bodyParts[LWrist].pixel.x > person.bodyParts[Neck].pixel.x > person.bodyParts[RWrist].pixel.x)
+                    and (person.bodyParts[LElbow].pixel.y > person.bodyParts[LShoulder].pixel.y > 0 )
+                    and (person.bodyParts[LElbow].pixel.y > person.bodyParts[LWrist].pixel.y > 0)
+                    and (person.bodyParts[LWrist].pixel.x - person.bodyParts[RWrist].pixel.x) < (person.bodyParts[LShoulder].pixel.x - person.bodyParts[RShoulder].pixel.x))
+
+    def stop_gesture(self, person):
+
+        return ((person.bodyParts[RElbow].pixel.y > person.bodyParts[RShoulder].pixel.y > 0) 
+                    and (person.bodyParts[RElbow].pixel.y > person.bodyParts[RWrist].pixel.y > 0)
+                    and (person.bodyParts[LWrist].pixel.x > person.bodyParts[Neck].pixel.x > person.bodyParts[RWrist].pixel.x)
+                    and (person.bodyParts[LElbow].pixel.y > person.bodyParts[LShoulder].pixel.y > 0 )
+                    and (person.bodyParts[LElbow].pixel.y > person.bodyParts[LWrist].pixel.y > 0)
+                    and (person.bodyParts[LWrist].pixel.x - person.bodyParts[RWrist].pixel.x) > (person.bodyParts[LShoulder].pixel.x - person.bodyParts[RShoulder].pixel.x))
 
 
-#! add probability and distance conditon in each statement accordingly!! 
-def go_forward_gesture(person):
-    return ((((person.bodyParts[LShoulder].pixel.y > person.bodyParts[LWrist].pixel.y > 0)
-                 or (person.bodyParts[RShoulder].pixel.y > person.bodyParts[RWrist].pixel.y > 0))
-                and not ((person.bodyParts[LShoulder].pixel.y > person.bodyParts[LWrist].pixel.y > 0)          #not both hand raised!
-                         and (person.bodyParts[RShoulder].pixel.y > person.bodyParts[RWrist].pixel.y > 0)))
-                and ((person.bodyParts[RShoulder].pixel.x > person.bodyParts[RWrist].pixel.x > 0)
-                     or (person.bodyParts[LWrist].x > person.bodyParts[LShoulder].pixel.x > 0))
-                and ((abs(person.bodyParts[RWrist].pixel.x - person.bodyParts[RElbow].pixel.x) < 100)           # this is the width/distance on x axis
-                    or (abs(person.bodyParts[LWrist].pixel.x - person.bodyParts[LElbow].pixel.x) < 100)))       # !elbow and wrist should be in line   
+    def go_right_gesture(self, person):
+        return ((0 < person.bodyParts[Neck].pixel.x < person.bodyParts[RWrist].pixel.x)
+                    and (0 < person.bodyParts[LShoulder].pixel.x < person.bodyParts[LElbow].pixel.x < person.bodyParts[LWrist].pixel.x)
+                    and (0 < person.bodyParts[LShoulder].pixel.y < person.bodyParts[LWrist].pixel.y)
+                    and (0 < person.bodyParts[RShoulder].pixel.y < person.bodyParts[RWrist].pixel.y))
 
 
-def go_backward_gesture(person):
+    def go_left_gesture(self, person):
+        return ((0 < person.bodyParts[LWrist].pixel.x < person.bodyParts[Neck].pixel.x)
+                    and (0 < person.bodyParts[RWrist].pixel.x < person.bodyParts[RElbow].pixel.x < person.bodyParts[RShoulder].pixel.x)
+                    and (0 < person.bodyParts[LShoulder].pixel.y < person.bodyParts[LWrist].pixel.y)
+                    and (0 < person.bodyParts[RShoulder].pixel.y < person.bodyParts[RWrist].pixel.y))
 
-    return ((person.bodyParts[RElbow].pixel.y > person.bodyParts[RShoulder].pixel.y > 0) 
-				and (person.bodyParts[RElbow].pixel.y > person.bodyParts[RWrist].pixel.y > 0)
-                and (person.bodyParts[LWrist].pixel.x > person.bodyParts[Neck].pixel.x > person.bodyParts[RWrist].pixel.x)
-                and (person.bodyParts[LElbow].pixel.y > person.bodyParts[LShoulder].pixel.y > 0 )
-                and (person.bodyParts[LElbow].pixel.y > person.bodyParts[LWrist].pixel.y > 0)
-                and (person.bodyParts[LWrist].pixel.x - person.bodyParts[RWrist].pixel.x) < (person.bodyParts[LShoulder].pixel.x - person.bodyParts[RShoulder].pixel.x))
+    def go_forward_with_right_turn(self, person):
+        return (0 < person.bodyParts[RShoulder].pixel.y < person.bodyParts[Neck].pixel.y < person.bodyParts[LShoulder].pixel.y
+            and 0 < person.bodyParts[RHip].pixel.x < person.bodyParts[RShoulder].pixel.x
+            and 0 < person.bodyParts[LHip].pixel.x < person.bodyParts[Neck].pixel.x)
 
-def stop_gesture(person):
+    
+    def go_forward_with_left_turn(self, person):
+        return (0 < person.bodyParts[LShoulder].pixel.y < person.bodyParts[Neck].pixel.y < person.bodyParts[RShoulder].pixel.y
+            and 0 < person.bodyParts[LShoulder].pixel.x < person.bodyParts[LHip].pixel.x
+            and 0 < person.bodyParts[Neck].pixel.x < person.bodyParts[RHip].pixel.x)
+    
 
-    return ((person.bodyParts[RElbow].pixel.y > person.bodyParts[RShoulder].pixel.y > 0) 
-				and (person.bodyParts[RElbow].pixel.y > person.bodyParts[RWrist].pixel.y > 0)
-                and (person.bodyParts[LWrist].pixel.x > person.bodyParts[Neck].pixel.x > person.bodyParts[RWrist].pixel.x)
-                and (person.bodyParts[LElbow].pixel.y > person.bodyParts[LShoulder].pixel.y > 0 )
-                and (person.bodyParts[LElbow].pixel.y > person.bodyParts[LWrist].pixel.y > 0)
-                and (person.bodyParts[LWrist].pixel.x - person.bodyParts[RWrist].pixel.x) > (person.bodyParts[LShoulder].pixel.x - person.bodyParts[RShoulder].pixel.x))
-
-
-    # return ((person.bodyParts[RElbow].pixel.y > person.bodyParts[RShoulder].pixel.y > person.bodyParts[RWrist].pixel.y > 0)     # RElbow > RShoulder and RElbow > Rwrist 
-    #             and (person.bodyParts[LWrist].pixel.x > person.bodyParts[Neck].pixel.x > person.bodyParts[RWrist].pixel.x)
-    #             and (person.bodyParts[LElbow].pixel.y > person.bodyParts[LShoulder].pixel.y > person.bodyParts[LWrist].pixel.y > 0)
-    #             and (person.bodyParts[LWrist].pixel.x - person.bodyParts[RWrist].pixel.x) > (person.bodyParts[LShoulder].pixel.x - person.bodyParts[RShoulder].pixel.x))
-
-
-def go_right_gesture(person):
-    return ((0 < person.bodyParts[Neck].pixel.x < person.bodyParts[RWrist].pixel.x)
-                and (0 < person.bodyParts[LShoulder].pixel.x < person.bodyParts[LElbow].pixel.x < person.bodyParts[LWrist].pixel.x)
-                and (0 < person.bodyParts[LShoulder].pixel.y < person.bodyParts[LWrist].pixel.y)
-                and (0 < person.bodyParts[RShoulder].pixel.y < person.bodyParts[RWrist].pixel.y))
-
-
-def go_left_gesture(person):
-    return ((0 < person.bodyParts[LWrist].pixel.x < person.bodyParts[Neck].pixel.x)
-                and (0 < person.bodyParts[RWrist].pixel.x < person.bodyParts[RElbow].pixel.x < person.bodyParts[RShoulder].pixel.x)
-                and (0 < person.bodyParts[LShoulder].pixel.y < person.bodyParts[LWrist].pixel.y)
-                and (0 < person.bodyParts[RShoulder].pixel.y < person.bodyParts[RWrist].pixel.y))
 
 
 def frame_callback(frame):
+
+
+    #!create the object instance of class Gestures() and movements()
+    Gestures = all_Gestures()
+    robot_movement = movements()
+
+
+    pub = rospy.Publisher('/gesture', Gesture, queue_size=10)
+    rate = rospy.Rate(10)  # 10Hz
+    gesture_msg = Gesture() #instance for Gesture msg
+
+
     # Extract frame details
     header = frame.header
     frame_id = header.frame_id
@@ -88,32 +164,62 @@ def frame_callback(frame):
         MidHip_distance = body_parts[MidHip].point.z
         MidHip_score    = body_parts[MidHip].score
 
-        threshold_confidence = 0.7
+        threshold_confidence = 0.8
         near_distance       = 1 # one meter
-        far_distance        = 4 # four meter
+        far_distance        = 2 # two meter
 
         if( (MidHip_score > threshold_confidence) and (near_distance < MidHip_distance < far_distance) ): #this criteria to choose a target person to whom bot will follow!
+            #rospy.loginfo('condition works!\n')
 
-            if go_forward_gesture(person): #if the condition defined in raise_hand fucntion is true if condition will get executed!
-                #TODO : create a fucntion where bot moves forward!
+            if Gestures.go_forward_gesture(person): #if the condition defined in raise_hand fucntion is true if condition will get executed!
                 # call fucntion to move robot
-                rospy.loginfo('GO Forward!\n')
+                #rospy.loginfo('GO Forward!\n')
+                gesture_msg.gesture = "Forward"
+                pub.publish(gesture_msg)
+                robot_movement.forward_movement()
 
-            elif stop_gesture(person):
+            elif Gestures.stop_gesture(person):
 
-                rospy.loginfo('STOP!!!!\n')
+                #rospy.loginfo('STOP!!!!\n')
+                gesture_msg.gesture = "STOP"
+                pub.publish(gesture_msg)
+                robot_movement.stop()
             
-            elif go_right_gesture(person):
+            elif Gestures.go_right_gesture(person):
 
-                rospy.loginfo('GO GO GO Right!\n')
+                #rospy.loginfo('GO GO GO Right!\n')
+                gesture_msg.gesture = "GO Right"
+                pub.publish(gesture_msg)
+                robot_movement.turn_right()
             
-            elif go_left_gesture(person):
+            elif Gestures.go_left_gesture(person):
 
-                rospy.loginfo('GO GO GO Left \n')
+                #rospy.loginfo('GO GO GO Left \n')
+                gesture_msg.gesture = "GO Left"
+                pub.publish(gesture_msg)
+                robot_movement.turn_left()
             
-            elif go_backward_gesture(person):
+            elif Gestures.go_backward_gesture(person):
 
-                rospy.loginfo('GO Backward \n')
+                #rospy.loginfo('GO Backward \n')
+                gesture_msg.gesture = "GO Backward"
+                pub.publish(gesture_msg)
+                robot_movement.backward_movement()
+
+            elif Gestures.go_forward_with_right_turn(person):
+
+                #rospy.loginfo('forward with right turn \n')
+                gesture_msg.gesture = "Forward+R_Turn"
+                pub.publish(gesture_msg)
+                robot_movement.forward_right_turn()
+
+            elif Gestures.go_forward_with_left_turn(person):
+
+                #rospy.loginfo('forward with left turn \n')
+                gesture_msg.gesture = "Forward+L_Turn"
+                pub.publish(gesture_msg)
+                robot_movement.forward_left_turn()
+
 
 
 def frame_subscriber():
@@ -121,28 +227,6 @@ def frame_subscriber():
     rospy.Subscriber('/frame', Frame, frame_callback)  
     rospy.spin()
 
-
 if __name__ == '__main__':
     frame_subscriber()
-
-
-            #TODO -> below to define gestures and commnad to bot. 
-            # if (body_parts[RShoulder].score > threshold_confidence and body_parts[RElbow].score > threshold_confidence and body_parts[RWrist].score > threshold_confidence):
-                
-            #     #* For Right Wrist
-            #     x_pixel_Rwrist    = body_parts[RWrist].pixel.x
-            #     y_pixel_Rwrist    = body_parts[RWrist].pixel.y
-            #     x_pixel_RElbow    = body_parts[RElbow].pixel.x
-            #     y_pixel_RElbow    = body_parts[RElbow].pixel.y
-            #     x_pixel_RShoulder    = body_parts[RShoulder].pixel.x
-            #     y_pixel_RShoulder    = body_parts[RShoulder].pixel.y
-            #     rospy.loginfo('x,y pixel Rwrist %f ,%f\n' , x_pixel_Rwrist, y_pixel_Rwrist)
-            #     rospy.loginfo('x,y pixel RElbow %f ,%f \n' , x_pixel_RElbow, y_pixel_RElbow)
-            #     rospy.loginfo('x,y pixel RShoulder %f, %f\n' , x_pixel_RShoulder, y_pixel_RShoulder)
-
-            #     rospy.loginfo('x,y pixel LWrist %f ,%f\n' , body_parts[LWrist].pixel.x, body_parts[LWrist].pixel.y)
-            #     rospy.loginfo('x,y pixel LElbow %f ,%f \n' , body_parts[LElbow].pixel.x, body_parts[LElbow].pixel.y)
-            #     rospy.loginfo('x,y pixel LShoulder %f, %f\n' , body_parts[LShoulder].pixel.x, body_parts[LShoulder].pixel.y)
-
-            #     rospy.loginfo('x,y pixel Neck %f ,%f\n' , body_parts[Neck].pixel.x, body_parts[Neck].pixel.y)
-            #     rospy.loginfo('x,y pixel MidHip %f ,%f \n' , body_parts[MidHip].pixel.x, body_parts[MidHip].pixel.y)
+    
